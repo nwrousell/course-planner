f74747f84afafc59a0498ba7c8ca70c1e8beaf30
nwrousell <59492853+nwrousell@users.noreply.github.com>
2024-02-03 21:46:10 -0500

    update validator

--- validator.ts
+++ validator.ts
@@ -1,21 +1,12 @@
 import Concentration_req from './Concentration_req.json'
-// Read concentration reqs json file
-// const CONCENTRATION_REQUIREMENTS = await ((await fetch("./Concentration_req.json")).json())
-console.log(Concentration_req)
 
-// set up classes the student has already taken
-const alreadyTaken = ["CSCI 0190", ""]
-
-// 
 type Requirement = {
     name: String
-    function: 'PICK' | 'AND' | 'OR' | 'ANY'
+    function: 'PICK' | 'AND' | 'OR' | 'ANY-ABOVE-1000'
     value: number | Requirement[]
     courses?: String[]
+    overlapAllowed?: boolean // AND-specific: can courses be used for both branches?
 }
-
-const course_list = () => JSON.parse(JSON.stringify(Concentration_req));
-const requirements = course_list["placeholder"];
 
 function min(a: number, b: number){
     if(a < b) return a
@@ -23,8 +14,8 @@ function min(a: number, b: number){
 }
 
 // Returns [boolean, courses_used, min_courses_left_to_satisfy]
-function isRequirementSatisfied(availableCourses: String[], requirement: Requirement) {
-    let leftResponse, rightResponse, isSatisfied, coursesUsed, minCoursesLeft
+function isRequirementSatisfied(availableCourses: string[], requirement: Requirement) {
+    let leftResponse, rightResponse, isSatisfied, coursesUsed, minCoursesLeft, foundCourse
     switch(requirement.function){
         case 'PICK':
             coursesUsed = []
@@ -32,12 +23,16 @@ function isRequirementSatisfied(availableCourses: String[], requirement: Require
             for(let i=0;i<requirement.value;i++){
                 // for each pick, find a course in availableCourses that is in requirement.courses
                 //@ts-ignore
+                foundCourse = false
                 for(let j=0;j<availableCourses.length;j++){
                     if(requirement.courses?.includes(availableCourses[j])){
                         coursesUsed.push(availableCourses[j])
                         availableCourses.splice(j, 1)
+                        foundCourse = true
+                        break
                     }
                 }
+                if(foundCourse) continue
                 // if we got here, we have no more courses that can satisfy the requirement
                 // @ts-ignore
                 minCoursesLeft = requirement.value - coursesUsed.length
@@ -49,6 +44,12 @@ function isRequirementSatisfied(availableCourses: String[], requirement: Require
               availableCourses,
               requirement["value"][0]
             );
+            availableCourses = availableCourses.filter((v) => !leftResponse[1].includes(v))
+            if(requirement.overlapAllowed){
+                // we want to use the same courses for both branches of the AND
+                // so front-load the coursesUsed of the leftResponse in availableCourses  
+                availableCourses = leftResponse[1] + availableCourses
+            }
             rightResponse = isRequirementSatisfied(
               availableCourses,
               requirement["value"][1]
@@ -56,7 +57,7 @@ function isRequirementSatisfied(availableCourses: String[], requirement: Require
 
             isSatisfied = leftResponse[0] && rightResponse[0]
             coursesUsed = [...new Set(leftResponse[1] + rightResponse[1])] // converting to a Set and back to remove duplicates
-            minCoursesLeft =  leftResponse[2] + rightResponse[2]
+            minCoursesLeft =  leftResponse[2] + rightResponse[2] // ! this won't work with overlap
             return [isSatisfied, coursesUsed, minCoursesLeft]
         case 'OR':
             leftResponse = isRequirementSatisfied(
@@ -72,16 +73,43 @@ function isRequirementSatisfied(availableCourses: String[], requirement: Require
             minCoursesLeft = min(leftResponse[2], rightResponse[2]);
             coursesUsed = (leftResponse[2]==minCoursesLeft) ? leftResponse[1] : rightResponse[1]
             return [isSatisfied, coursesUsed, minCoursesLeft];
+        case 'ANY-ABOVE-1000':
+            coursesUsed = []
+            let department, number
+            // @ts-ignore
+            for(let i=0;i<requirement.value;i++){
+                // for each pick, find a course in availableCourses that is 1000-level and of department courses[0]
+                //@ts-ignore
+                foundCourse = false
+                for(let j=0;j<availableCourses.length;j++){
+                    department = availableCourses[j].split[0]
+                    number = parseInt(availableCourses[j].split[1])
+                    // @ts-ignore
+                    if(department == requirement.courses[0] && number >= 1000){
+                        coursesUsed.push(availableCourses[j])
+                        availableCourses.splice(j, 1)
+                        foundCourse = true
+                        break
+                    }
+                }
+                if(foundCourse) continue
+                // if we got here, we have no more courses that can satisfy the requirement
+                // @ts-ignore
+                minCoursesLeft = requirement.value - coursesUsed.length
+                return [false, coursesUsed, minCoursesLeft]
+            }
+            return [true, coursesUsed, 0]
     }
 }
 
-function checkall(courses_taken : Array<String>){
-    const course_list = () => JSON.parse(JSON.stringify(Concentration_req));
-    const requirements : Object = course_list["CS"]["AB"];
-    for (const key in requirements){
-        isRequirementSatisfied(courses_taken, requirements[key]);
-    }
-}
+console.log(Concentration_req)
+
+// set up classes the student has already taken
+const alreadyTaken = ["CSCI 0190", "CSCI 0300", "ECON 1110", "ECON 0110", 
+"CSCI 1951A", "CLPS 0220", "CLPS 0700", "MATH 0520", "MATH 0200", "MATH 0100", "CSCI 2470", "CSCI 1430", "CSCI 1951Z", "HIST 0234", "CSCI 0220"]
+
+// ! CS19's second slot will take the next available CS, which isn't always the best choice
+// ! (it could take cs300, which then can't be used for intermediate courses)
 
 // To compile: tsc .\validator.ts --resolveJsonModule --module nodenext
 // to run node validator.js
